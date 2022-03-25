3b4b7140f42d70d33a6cf3bea9e7bc41d20f92dd
Ozmar Escobar <25725331+ZantetsukenGT@users.noreply.github.com>
2022-03-25 00:00:47 -0600

    CreateClient: se pide name y price, se genera uuid y se inserta

--- endpoints/endpoints-impl/UserCRUD/CreateClient.js
+++ endpoints/endpoints-impl/UserCRUD/CreateClient.js
@@ -1,44 +1,18 @@
 const { isEmpty } = require('validator')
-const mysql = require('mysql')
+const { v4 } = require('uuid')
 
-const endpointsImplementation = {}
-endpointsImplementation.createClient = async (req, res) =>
+const endpoint = async (req, res) =>
 {
-    let { name, lastname, email } = req.body
-    
+    let { name, price } = req.body
     const iw = { ignore_whitespaces: true }
     
-    if((!name || isEmpty(name, iw)) && (!lastname || isEmpty(lastname, iw)) && (!email || isEmpty(email, iw)))
-        return res.status(401).send({ msg:'No se recibió información para actualizar.' })
+    const namePresent = name && !isEmpty(name, iw)
+    const pricePresent = price !== undefined
+    if(!namePresent || !pricePresent)
+        return res.status(400).send({ msg:'Faltan campos obligatorios.' })
         
-    const mysqlConnection = mysql.createConnection(req.mysqlOptions)
-    try
-    {
-        mysqlConnection.connect((err) =>
-        {
-            if (err)
-            {
-                console.error('Error al conectar a mysql:')
-                throw err
-            }
-            console.log('Éxito al conectar a mysql.')
-        })
-        
-        mysqlConnection.query('INSERT INTO usuarios (nombre, apellido, correo) VALUES (?,?,?)', [name, lastname, email], (err, results, fields) =>
-        {
-            if (err) throw err
-            return res.status(200).send({ msg:'Datos insertados correctamente.' })
-        })
-    }
-    catch(ex)
-    {
-        console.log(ex)
-        return res.status(500).send({ msg:'Error al actualizar la información, por favor intente más tarde.' })
-    }
-    finally
-    {
-        mysqlConnection.end()
-    }
+    req.productos.push({_id: v4(), name, price})
+    res.status(200).send({ msg:'Se inserto correctamente' })
 }
 
-module.exports = endpointsImplementation
+module.exports = endpoint
