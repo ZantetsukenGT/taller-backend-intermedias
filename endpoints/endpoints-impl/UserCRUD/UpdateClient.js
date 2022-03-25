f05fc4cb9749f9414b8ef95978286a079cbee61b
Ozmar Escobar <25725331+ZantetsukenGT@users.noreply.github.com>
2022-03-25 00:02:21 -0600

    UpdateClient: se pide id, debe venir al menos name y precio y se actualiza

--- endpoints/endpoints-impl/UserCRUD/UpdateClient.js
+++ endpoints/endpoints-impl/UserCRUD/UpdateClient.js
@@ -1,47 +1,30 @@
 const { isEmpty } = require('validator')
-const mysql = require('mysql')
 
-const endpointsImplementation = {}
-endpointsImplementation.updateClient = async (req, res) =>
+const endpoint = async (req, res) =>
 {
-    let { id, name, lastname, email } = req.body
+    let { _id } = req.params
+    let { name, price } = req.body
 
     const iw = { ignore_whitespaces: true }
 
-    if (!id || isEmpty(id, iw))
+    if (!_id || isEmpty(_id, iw))
         return res.status(400).send({ msg:'Faltan campos obligatorios.' })
 
-    if((!name || isEmpty(name, iw)) && (!lastname || isEmpty(lastname, iw)) && (!email || isEmpty(email, iw)))
+    const nameForUpdate = name && !isEmpty(name, iw)
+    const priceForUpdate = price !== undefined
+    if(!nameForUpdate && !priceForUpdate)
         return res.status(401).send({ msg:'No se recibió información para actualizar.' })
 
-    const mysqlConnection = mysql.createConnection(req.mysqlOptions)
-    try
-    {
-        mysqlConnection.connect((err) =>
+    req.productos.map(value => {
+        if (value._id == _id)
         {
-            if (err)
-            {
-                console.error('Error al conectar a mysql:')
-                throw err
-            }
-            console.log('Éxito al conectar a mysql.')
-        })
-
-        mysqlConnection.query('UPDATE usuarios SET nombre = ?, apellido = ?, correo = ? WHERE id = ?', [name, lastname, email, id], (err, results, fields) =>
-        {
-            if (err) throw err
-            return res.status(200).send({ msg:'Datos actualizados correctamente.' })
-        })
-    }
-    catch (ex)
-    {
-        console.log(ex)
-        return res.status(500).send({ msg:'Error al actualizar la información, por favor intente más tarde.' })
-    }
-    finally
-    {
-        mysqlConnection.end()
-    }
+            if (nameForUpdate)
+                value.name = name
+            if (priceForUpdate)
+                value.price = price
+        }
+    })
+    res.status(200).send({ msg:'Se actualizo correctamente' })
 }
 
-module.exports = endpointsImplementation
+module.exports = endpoint
